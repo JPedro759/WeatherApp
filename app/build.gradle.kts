2e44440d427cafffb6c3629e1ec618463077fe54
JPedro759 <joaopedromeloo03@gmail.com>
2024-10-15 20:54:20 -0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.example.weatherapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.weatherapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.androidX)
+    implementation(platform(libs.compose.bom))
+    debugImplementation(libs.compose.tooling)
+    implementation(libs.bundles.ui)
+}
